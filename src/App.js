212197fe7b1f35082d1a2c4b4b0555db362f94c6
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-10-02 16:58:56 +1100

    login page styled

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Login from './Login';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* spotify logo */}
+      {/* Login with spotify button */}
+      <Login />
+
+    </div>
+  );
+}
+
+export default App;
